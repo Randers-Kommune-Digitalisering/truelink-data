d326fa042f6aa23e33ead4f0f09006d3f3533808
Rune Aagaard Keena <144774061+rak-randers@users.noreply.github.com>
2023-12-20 09:27:11 +0100

    Initial commit

---
+++ node-red/flows_js/comment.013cab8d59f75d48.flows.js
@@ -0,0 +1,12 @@
+const Node = {
+  "id": "013cab8d59f75d48",
+  "type": "comment",
+  "z": "971a7ae6df987a48",
+  "name": "Time to start coding!",
+  "info": "",
+  "x": 150,
+  "y": 60,
+  "wires": []
+}
+
+module.exports = Node;
---
+++ node-red/flows_js/tab.971a7ae6df987a48.flows.js
@@ -0,0 +1,10 @@
+const Node = {
+  "id": "971a7ae6df987a48",
+  "type": "tab",
+  "label": "Flow 1",
+  "disabled": false,
+  "info": "",
+  "env": []
+}
+
+module.exports = Node;
