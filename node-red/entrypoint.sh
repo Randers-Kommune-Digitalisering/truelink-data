d3ae3f33090dcd2125a2a1ea273ff09ca52e6609
rak-randers <rune.aagaard.keena@randers.dk>
2023-12-21 09:02:47 +0100

    setting key file for SFTP

--- node-red/entrypoint.sh
+++ node-red/entrypoint.sh
@@ -8,7 +8,7 @@ function stop() {
 }
 
 # Decode base64 key data and write it to shh key file in SFTP module path
-echo "$SSH_KEY" | base64 -d > "$SFTP_MODULE_PATH/$SFTP_SSH_KEY_FILE"
+echo "$SSH_KEY_BASE64" | base64 -d > "./$SFTP_MODULE_PATH/$SFTP_SSH_KEY_FILE"
 
 /usr/local/bin/node $NODE_OPTIONS node_modules/node-red/red.js --userDir /data $FLOWS "${@}" &
 
